bfa6baab4fd5366e73ec35def8c004cbe191d388
Kim Huynh <kimhuynh@gatech.edu>
2021-06-03 20:44:49 -0400

    Better error handling.

--- app/src/main/java/com/khd/chess/MainActivity.java
+++ app/src/main/java/com/khd/chess/MainActivity.java
@@ -31,21 +31,25 @@ public class MainActivity extends AppCompatActivity {
     *
     */
     public boolean initTimer(){
-        textViewTimer = (TextView) findViewById(R.id.text_view_timer);
-        CountDownTimer countDownTimer = new CountDownTimer(5000, 100) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                String secondsLeft = String.valueOf((int)millisUntilFinished/1000);
-                textViewTimer.setText(secondsLeft);
-                System.out.println(secondsLeft);
-            }
+        try{
+            textViewTimer = (TextView) findViewById(R.id.text_view_timer);
+            CountDownTimer countDownTimer = new CountDownTimer(5000, 100) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    String secondsLeft = String.valueOf((int)millisUntilFinished/1000);
+                    textViewTimer.setText(secondsLeft);
+                }
 
-            @Override
-            public void onFinish() {
-                System.out.println("Time Completed.");
-                textViewTimer.setText("Time Completed.");
-            }
-        }.start();
+                @Override
+                public void onFinish() {
+                    textViewTimer.setText("Time Completed.");
+                }
+            }.start();
+        }catch(Exception e){
+            System.err.println("Failed to initialize timer. " + e);
+            return false;
+        }
+
         return true;
     }
 
@@ -55,27 +59,35 @@ public class MainActivity extends AppCompatActivity {
      * @param
      */
     public boolean initTable(){
-        tableLayoutChess = (TableLayout)findViewById(R.id.table_chess);
-        tableLayoutChess.setPadding(80,200,80,200);
-        int count = 0;
-        for(int i=0; i<8; i++){
-            TableRow row = new TableRow(this);
-            tableLayoutChess.addView(row);
-            row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 1.0f));
-            for(int j=0; j<8; j++) {
-                TableRow col = new TableRow(this);
-                if((count%2)==0){col.setBackgroundColor(Color.WHITE);}
-                else{col.setBackgroundColor(Color.BLACK);}
-                TextView str2 = new TextView(this);
-                str2.setTextColor(Color.BLUE);
-                str2.setText(i + ", " + j);
-                col.addView(str2);
-                row.addView(col);
-                col.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
-                count++;
-                System.out.println("Testing.");
+        try {
+            tableLayoutChess = (TableLayout) findViewById(R.id.table_chess);
+            tableLayoutChess.setPadding(80, 200, 80, 200);
+            int count = 0;
+            for (int i = 0; i < 8; i++) {
+                TableRow row = new TableRow(this);
+                tableLayoutChess.addView(row);
+                row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 1.0f));
+                for (int j = 0; j < 8; j++) {
+                    TableRow col = new TableRow(this);
+                    if ((count % 2) == 0) {
+                        col.setBackgroundColor(Color.WHITE);
+                    } else {
+                        col.setBackgroundColor(Color.BLACK);
+                    }
+                    TextView str2 = new TextView(this);
+                    str2.setTextColor(Color.BLUE);
+                    str2.setText(i + ", " + j);
+                    col.addView(str2);
+                    row.addView(col);
+                    col.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
+                    count++;
+                    System.out.println("Testing.");
+                }
             }
-        }
+        }catch(Exception e){
+        System.err.println("Failed to initialize table. " + e);
+        return false;
+    }
         return true;
     }
 }

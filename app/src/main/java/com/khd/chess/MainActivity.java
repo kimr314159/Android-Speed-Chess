af6a4a1824ecf90a91037808f413409a709a651e
Kim Huynh <kimhuynh@gatech.edu>
2021-06-06 16:59:15 -0400

    eliminate second loop and show pieces on table.

--- app/src/main/java/com/khd/chess/MainActivity.java
+++ app/src/main/java/com/khd/chess/MainActivity.java
@@ -68,38 +68,6 @@ public class MainActivity extends AppCompatActivity {
             tableLayoutChess = (TableLayout) findViewById(R.id.table_chess);
             tableLayoutChess.setPadding(80, 200, 80, 200);
 
-
-            //Init entire table
-            int count = 0;
-            for (int i = 0; i < 8; i++) {
-                count++;
-                TableRow row = new TableRow(this);
-                tableLayoutChess.addView(row);
-                row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 1.0f));
-                for (int j = 0; j < 8; j++) {
-
-
-                    TableRow col = new TableRow(this);
-                    if ((count % 2) == 0) {
-                        col.setBackgroundColor(Color.parseColor("#47597F"));
-                    } else {
-                        col.setBackgroundColor(Color.parseColor("#4C6284"));
-                    }
-                    TextView str2 = new TextView(this);
-                    str2.setTextColor(Color.WHITE);
-                    str2.setText(j + ", " + i);
-                    col.addView(str2);
-                    row.addView(col);
-
-                    col.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
-                    count++;
-                    System.out.println("Testing.");
-                }
-            }
-
-
-
-
             //Define 2d array
             System.out.println(getClass());
             Piece[][] sideA = new Piece[2][8];
@@ -110,20 +78,59 @@ public class MainActivity extends AppCompatActivity {
             sideA[0][4] = king;
             sideA[0][6] = knightB;
 
-            //
-            ImageView imageView = new ImageView(this);
-            imageView.setImageResource(R.drawable.king);
-            //
+
+            //Init entire table
+            int count = 0;
+            for (int i = 0; i < 8; i++) {
+                count++;
+                TableRow row = new TableRow(this);
+                tableLayoutChess.addView(row);
+                row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 1.0f));
+                for (int j = 0; j < 8; j++) {
+                    TableRow col = new TableRow(this);
+                    if ((count % 2) == 0) {
+                        col.setBackgroundColor(Color.parseColor("#47597F"));
+                    } else {
+                        col.setBackgroundColor(Color.parseColor("#4C6284"));
+                    }
+                    TextView str2 = new TextView(this);
+                    str2.setTextColor(Color.WHITE);
+                    str2.setText(j + ", " + i);
+                    row.addView(col);
+
+                    if(i<sideA.length&&sideA[i][j]!=null){
+                        ImageView imageView = sideA[i][j].getImageView(this);
+
+                        col.addView(imageView);
+                        imageView.getLayoutParams().height = 100;
+                        imageView.getLayoutParams().width = 100;
+                        imageView.requestLayout();
+
+                        System.out.println(j + ", " + i);
+                        System.out.println(sideA[i][j]);
+                        System.out.println("not null");
+                    }else{
+                        col.addView(str2);
+                    }
+
+                    col.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
+                    count++;
+
+
+                    System.out.println("Testing.");
+                }
+            }
+
+
+
+
+
+
 
             //Init black pieces
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 8; j++) {
-                    if(sideA[i][j]!=null){
-                        sideA[i][j].getImageView(this);
-                        System.out.println(j + ", " + i);
-                        System.out.println(sideA[i][j]);
-                        System.out.println("not null");
-                    }
+
                 }
             }
         }catch(Exception e){

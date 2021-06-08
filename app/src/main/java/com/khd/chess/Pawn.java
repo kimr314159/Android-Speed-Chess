e3f274947bf6c29a500d627bcacff579cf1793ed
Kim Huynh <kimhuynh@gatech.edu>
2021-06-07 20:54:21 -0400

    start pawn class using interface.

---
+++ app/src/main/java/com/khd/chess/Pawn.java
@@ -0,0 +1,38 @@
+package com.khd.chess;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.widget.ImageView;
+
+public class Pawn implements Piece {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public boolean setImagepath() {
+        return false;
+    }
+
+    @Override
+    public String getImagePath() {
+        return null;
+    }
+
+    @Override
+    public void getAllowedMoves() {
+
+    }
+
+    @Override
+    public ImageView getImageView(Context context) {
+        return null;
+    }
+}

c384ecaed884ae79ddcbad9b8e6af115670298b2
Kim Huynh <kimhuynh@gatech.edu>
2021-06-09 23:05:36 -0400

    remove default tests.

---
+++ app/src/androidTest/java/com/khd/chess/InstrumentedTest.java
@@ -0,0 +1,15 @@
+package com.khd.chess;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTest {
+}
---
+++ app/src/test/java/com/khd/chess/UnitTest.java
@@ -0,0 +1,8 @@
+package com.khd.chess;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+}
